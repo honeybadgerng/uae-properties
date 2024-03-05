10a81dc9d410dbec3ac8f7f0305bb5822e6e533a
Honeybadger <rajimoshood200482@gmail.com>
2024-03-05 15:12:45 +0100

    updated to use environmental variables

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
       "x-rapidapi-host": "bayut.p.rapidapi.com",
-      "x-rapidapi-key": "471ebba5a3msh9dfc2c3ed197ed0p17f530jsn08c32a7440c6",
+      "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPID_API_KEY,
     },
   });
 
